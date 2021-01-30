1b4db9d62becb07360250ac681d1a4edc6878d1c
bwnstck <benjamin@weinstock.it>
2021-01-30 21:29:13 +0100

    :art: Add Bio Length without styling

--- src/screens/AboutMe.tsx
+++ src/screens/AboutMe.tsx
@@ -1,70 +1,131 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import styled from "styled-components/macro";
 // import ChristophClimbing from "../assets/christoph_climbing.jpg";
 import Christoph from "../assets/profilePicBoss.jpg";
 import StyledSection from "../components/StyledSection";
 
 const AboutMe: FunctionComponent = () => {
+  const [textLength, setTextLength] = useState("s");
+
   return (
     <AboutMeWrapper>
       <h1>Über mich:</h1>
-      <img src={Christoph} alt="Christoph auf einem Baum" />
-      <div>
-        <p>
-          In der <b>Region Heilbronn</b> aufgewachsen war ich schon als Kind in
-          der Natur unterwegs. Der Pfühlpark, der Neckar oder der Heilbronner
-          Wald waren in Laufrunden oder bei Kinderfreizeiten immer wieder meine
-          Ziele. Mein Interesse an Umwelt und Natur entwickelte sich und wurde
-          durch Projekte mit Umweltorganisationen gestärkt. Durch ein
-          ”Freiwilliges ökologisches Jahr“ auf dem Waldheim Gaffenberg entdeckte
-          ich mein Interesse an Bäumen. Nicht nur die Größe, auch ihre Eigenart
-          und die vielfältigen Standorte sind immer wieder bewundernswert. So
-          fasste ich den Entschluss, an der HAWK in Göttingen Arboristik zu
-          studieren.
-        </p>
-        <p>
-          Durch meinen drei Studienjahren wurden die biologischen
-          Grundkenntnisse vertieft. Zum Studienprogramm gehörten Diskussionen
-          über das Thema Bäume in der Stadt: Welche Aufgaben übernehmen sie?
-          Welche Ansprüche haben sie? Die Fachgespräche brachten mir einen
-          weiten Blick für das urbane Grün.
-        </p>
-        <p>
-          Während den Semesterferien bildete ich mich ständig praktisch fort. Im
-          ersten Sommer machte ich den Kletterschein. Das ist die Voraussetzung,
-          beruflich mit Seilklettertechnik und Handsäge im Baum zu arbeiten. In
-          den weiteren Ferien arbeitete ich zusammen mit Studienkollegen in der
-          Baumpflege und machte Praktika bei unterschiedlichen Baumpflegefirmen
-          in ganz Deutschland. Die Umsetzung der Theorie in die Praxis war am
-          Anfang nicht so einfach, doch langsam entwickelte sich ein anderer
-          Blick. Ich konnte an den ersten Bäumen, die ich gepflegt habe, die
-          Reaktionen auf meine Schnittmaßnahmen beobachten und auswerten.
-        </p>
-        <p>
-          Nach dem Studienabschluss festigte ich mein Wissen und sammelte
-          zusätzlich praktische Erfahrungen bei der Ausbildung zum Fachagrarwirt
-          für Baumpflege und Baumsanierung an der Landesversuchsanstalt für
-          Gartenbau in Heidelberg.
-        </p>
-        <p>
-          Nach Abschluss meiner Ausbildungen bleibt mir der Kontakt mit vielen
-          anderen Baumpflegern wichtig. Er vermittelt neue Erfahrungen und
-          fördert die Beschäftigung mit Forschung und Wissenschaft im
-          Fachbereich Arboristik.
-        </p>
-        <p>
-          Der Baum übernimmt viele Aufgaben in der Stadt. Er verbessert das
-          Stadtklima, filtert Feinstaub, dient als Sichtschutz oder
-          Schattenspender. Er bringt die Natur in die Stadt. Parkanlagen mit
-          Bäumen schaffen Räume für Freude und Erholung. Für viele Tiere ist der
-          Baum ein wichtiger oder der einzige Lebensraum, den sie als
-          Nahrungsgrundlage, Schlaf-, Ruhe- oder Nistplatz brauchen.
-        </p>
-        <p>
-          2016 gründete ich meine Baumpflegefirma Lemur, um den Wuchs der Bäume
-          zu fördern und den besten Ausgleich zwischen Natur und Stadt, Baum und
-          Mensch zu erreichen!
-        </p>
+      <img src={Christoph} alt="Christoph as a Gangmember" />
+      <h2>Bio Length</h2>
+      <BioLength>
+        <div className="RadioButton">
+          <label htmlFor="s">
+            <input
+              type="radio"
+              name="s"
+              checked={textLength === "s"}
+              onClick={() => setTextLength("s")}
+            />
+          </label>
+        </div>
+        <hr />
+        <div className="RadioButton">
+          <label htmlFor="m">
+            <input
+              type="radio"
+              name="m"
+              checked={textLength === "m"}
+              onClick={() => setTextLength("m")}
+            />
+          </label>
+        </div>
+        <hr />
+        <div className="RadioButton">
+          <label htmlFor="l">
+            <input
+              type="radio"
+              name="l"
+              checked={textLength === "l"}
+              onClick={() => setTextLength("l")}
+            />
+          </label>
+        </div>
+      </BioLength>
+      <TextBox>
+        {textLength === "s" ? (
+          <>
+            <p>Hi ich bin Christoph und liebe Bäume und Mensch!</p>
+          </>
+        ) : textLength === "m" ? (
+          <>
+            <p>
+              In der <b>Region Heilbronn</b> aufgewachsen war ich schon als Kind
+              in der Natur unterwegs. Der Pfühlpark, der Neckar oder der
+              Heilbronner Wald waren in Laufrunden oder bei Kinderfreizeiten
+              immer wieder meine Ziele. Mein Interesse an Umwelt und Natur
+              entwickelte sich und wurde durch Projekte mit Umweltorganisationen
+              gestärkt. Durch ein ”Freiwilliges ökologisches Jahr“ auf dem
+              Waldheim Gaffenberg entdeckte ich mein Interesse an Bäumen. Nicht
+              nur die Größe, auch ihre Eigenart und die vielfältigen Standorte
+              sind immer wieder bewundernswert. So fasste ich den Entschluss, an
+              der HAWK in Göttingen Arboristik zu studieren.
+            </p>
+          </>
+        ) : (
+          <>
+            <p>
+              In der <b>Region Heilbronn</b> aufgewachsen war ich schon als Kind
+              in der Natur unterwegs. Der Pfühlpark, der Neckar oder der
+              Heilbronner Wald waren in Laufrunden oder bei Kinderfreizeiten
+              immer wieder meine Ziele. Mein Interesse an Umwelt und Natur
+              entwickelte sich und wurde durch Projekte mit Umweltorganisationen
+              gestärkt. Durch ein ”Freiwilliges ökologisches Jahr“ auf dem
+              Waldheim Gaffenberg entdeckte ich mein Interesse an Bäumen. Nicht
+              nur die Größe, auch ihre Eigenart und die vielfältigen Standorte
+              sind immer wieder bewundernswert. So fasste ich den Entschluss, an
+              der HAWK in Göttingen Arboristik zu studieren.
+            </p>
+            <p>
+              Durch meinen drei Studienjahren wurden die biologischen
+              Grundkenntnisse vertieft. Zum Studienprogramm gehörten
+              Diskussionen über das Thema Bäume in der Stadt: Welche Aufgaben
+              übernehmen sie? Welche Ansprüche haben sie? Die Fachgespräche
+              brachten mir einen weiten Blick für das urbane Grün.
+            </p>
+            <p>
+              Während den Semesterferien bildete ich mich ständig praktisch
+              fort. Im ersten Sommer machte ich den Kletterschein. Das ist die
+              Voraussetzung, beruflich mit Seilklettertechnik und Handsäge im
+              Baum zu arbeiten. In den weiteren Ferien arbeitete ich zusammen
+              mit Studienkollegen in der Baumpflege und machte Praktika bei
+              unterschiedlichen Baumpflegefirmen in ganz Deutschland. Die
+              Umsetzung der Theorie in die Praxis war am Anfang nicht so
+              einfach, doch langsam entwickelte sich ein anderer Blick. Ich
+              konnte an den ersten Bäumen, die ich gepflegt habe, die Reaktionen
+              auf meine Schnittmaßnahmen beobachten und auswerten.
+            </p>
+            <p>
+              Nach dem Studienabschluss festigte ich mein Wissen und sammelte
+              zusätzlich praktische Erfahrungen bei der Ausbildung zum
+              Fachagrarwirt für Baumpflege und Baumsanierung an der
+              Landesversuchsanstalt für Gartenbau in Heidelberg.
+            </p>
+            <p>
+              Nach Abschluss meiner Ausbildungen bleibt mir der Kontakt mit
+              vielen anderen Baumpflegern wichtig. Er vermittelt neue
+              Erfahrungen und fördert die Beschäftigung mit Forschung und
+              Wissenschaft im Fachbereich Arboristik.
+            </p>
+            <p>
+              Der Baum übernimmt viele Aufgaben in der Stadt. Er verbessert das
+              Stadtklima, filtert Feinstaub, dient als Sichtschutz oder
+              Schattenspender. Er bringt die Natur in die Stadt. Parkanlagen mit
+              Bäumen schaffen Räume für Freude und Erholung. Für viele Tiere ist
+              der Baum ein wichtiger oder der einzige Lebensraum, den sie als
+              Nahrungsgrundlage, Schlaf-, Ruhe- oder Nistplatz brauchen.
+            </p>
+            <p>
+              2016 gründete ich meine Baumpflegefirma Lemur, um den Wuchs der
+              Bäume zu fördern und den besten Ausgleich zwischen Natur und
+              Stadt, Baum und Mensch zu erreichen!
+            </p>
+          </>
+        )}
 
         <p>
           Sie haben noch Fragen?
@@ -73,11 +134,22 @@ const AboutMe: FunctionComponent = () => {
           <a href="mailto:info@lemurbaumpflege.de">in Verbindung.</a>
         </p>
         <span>Ihr Christoph Mössinger</span>
-      </div>
+      </TextBox>
     </AboutMeWrapper>
   );
 };
-
+const BioLength = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  margin: auto auto 1rem;
+  hr {
+    width: 3rem;
+    color: red;
+    height: 2px;
+  }
+`;
 const AboutMeWrapper = styled(StyledSection)`
   text-align: center;
   img {
@@ -86,14 +158,17 @@ const AboutMeWrapper = styled(StyledSection)`
     margin: 1rem auto;
     max-width: 350px;
   }
-  div {
-    background-color: var(--text-primary);
-    padding: 1rem;
-    border-radius: 5px;
+  div:nth-of-type(2) {
   }
   p {
     margin-bottom: 2rem;
     text-align: left;
   }
+`;
+
+const TextBox = styled.div`
+  background-color: var(--text-primary);
+  padding: 1rem;
+  border-radius: 5px;
 `;
 export default AboutMe;
